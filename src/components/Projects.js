983e0af9bc6025ea0130bae6e7195258b5af7173
Stacy Martin <sbrown1031@gmail.com>
2021-07-01 11:39:37 -0500

    carousel functioning

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,83 @@
 import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import Boredify from "../assets/images/boredify.png";
+import budgetTracker from "../assets/images/budget tracker.png";
+import employeeDirectory from "../assets/images/employee directory.png";
+import wagemtagem from "../assets/images/wagemtagem.png";
+import weatherDashboard from "../assets/images/weather dashboard.png"
+import "./styles.css";
 
+function Projects(props) {
+  return (
+    <div>
+      <Carousel className="carouselContainer">
+        <Carousel.Item>
+          <img
+            className="d-block w-100 carouselItem"
+            src={Boredify}
+            alt="boredify img"
+          />
+          <Carousel.Caption>
+            <h3>Boredify: An app to escape boredom</h3>
+            <a href="https://cbmartinez42.github.io/boredify-project/">https://cbmartinez42.github.io/boredify-project/</a>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-function Projects (props) {
-    return (
-        <div>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={wagemtagem}
+            alt="wagemtagem img"
+          />
+          <Carousel.Caption>
+            <h3>Wag Em Tag Em: A place where like minded canines can link</h3>
+            <p>https://limitless-caverns-66584.herokuapp.com/</p>
+          </Carousel.Caption>
+        </Carousel.Item>
 
-        </div>
-    )
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={weatherDashboard}
+            alt="weather dashboard img"
+          />
+          <Carousel.Caption>
+            <h3>Weather Dashboard: A dynamic weather app</h3>
+            <p>https://stacy-martin.github.io/weather-dashboard/</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={employeeDirectory}
+            alt="Employee Directory img"
+          />
+          <Carousel.Caption>
+            <h3>
+              Employee Directory: React app for quick access to employee
+              databases
+            </h3>
+            <p>https://stacy-martin.github.io/employee_directory/</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={budgetTracker}
+            alt="budget tracker img"
+          />
+          <Carousel.Caption>
+            <h3>
+              Budget Tracker: Personal accounting app with offline functioning
+            </h3>
+            <p>https://whispering-scrubland-98765.herokuapp.com/</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
 }
 
 export default Projects;
