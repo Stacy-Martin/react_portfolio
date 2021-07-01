c29d58200dbca3e62142379332c68d5dc5794ff4
Stacy Martin <sbrown1031@gmail.com>
2021-06-30 22:52:30 -0500

    setup file structure

---
+++ src/components/Bio.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Bio (props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Bio;
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Projects (props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Projects;
